97f9e6b24e7215fbbd5565c1987d6f8080d4f369
Jonathan Yeh <johnnyyeh0723@gmail.com>
2024-07-02 19:40:07 +0800

    Update TestModel.py

--- starter_code/part1-transfer/TestModel.py
+++ starter_code/part1-transfer/TestModel.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def imshow(inp, title=None):
     """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
+    inp = inp.cpu().numpy().transpose((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
     inp = std * inp + mean
